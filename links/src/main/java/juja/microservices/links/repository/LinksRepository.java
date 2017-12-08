771807a7570b3f145efe4f9f2fbaf61ca088ea49
Ivan Shapovalov <ivshapovalov@gmail.com>
2017-12-08 14:05:28 +0300

    #12 Create project structure - add Repo

---
+++ links/src/main/java/juja/microservices/links/repository/LinksRepository.java
@@ -0,0 +1,12 @@
+package juja.microservices.links.repository;
+
+import juja.microservices.links.model.Link;
+
+/**
+ * @author Ivan Shapovalov
+ */
+public interface LinksRepository {
+
+    Link saveLink();
+
+}
---
+++ links/src/main/java/juja/microservices/links/repository/impl/LinksRepositoryImpl.java
@@ -0,0 +1,15 @@
+package juja.microservices.links.repository.impl;
+
+import juja.microservices.links.model.Link;
+import juja.microservices.links.repository.LinksRepository;
+
+/**
+ * @author Ivan Shapovalov
+ */
+public class LinksRepositoryImpl implements LinksRepository {
+
+    @Override
+    public Link saveLink() {
+        return null;
+    }
+}
