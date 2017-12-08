947ac8ada885f75ecf594284008c679596e8142d
Ivan Shapovalov <ivshapovalov@gmail.com>
2017-12-08 09:35:54 +0300

    #3 Create base configuration for Links service

---
+++ links/src/main/java/juja/microservices/links/Application.java
@@ -0,0 +1,15 @@
+package juja.microservices.links;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Danil Kuznetsov (kuznetsov.danil.v@gmail.com)
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
