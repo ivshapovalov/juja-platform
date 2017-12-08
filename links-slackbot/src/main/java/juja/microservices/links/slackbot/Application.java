8a4f376870bd0c30d512d551214fe45d42a1e538
Ivan Shapovalov <ivshapovalov@gmail.com>
2017-12-08 10:39:39 +0300

    #5 Create project structure - Add model

--- links-slackbot/src/main/java/juja/microservices/links/slackbot/Application.java
+++ links-slackbot/src/main/java/juja/microservices/links/slackbot/Application.java
@@ -1,15 +1,41 @@
 package juja.microservices.links.slackbot;
 
-
+import org.apache.http.impl.client.HttpClients;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
 
-/**
- * @author Danil Kuznetsov (kuznetsov.danil.v@gmail.com)
- */
+import java.util.ArrayList;
+import java.util.List;
+
+
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        RestTemplate restTemplate = new RestTemplate(httpRequestFactory());
+        restTemplate.setMessageConverters(getHttpMessageConverters());
+        return restTemplate;
+    }
+
+    private ClientHttpRequestFactory httpRequestFactory() {
+        return new HttpComponentsClientHttpRequestFactory(HttpClients.createDefault());
+    }
+
+    private List<HttpMessageConverter<?>> getHttpMessageConverters() {
+        List<HttpMessageConverter<?>> converters = new ArrayList<>();
+        converters.add(new MappingJackson2HttpMessageConverter());
+        converters.add(new StringHttpMessageConverter());
+        return converters;
     }
 }
---
+++ links-slackbot/src/test/java/juja/microservices/links/slackbot/integration/LinksSlackbotIntegrationTest.java
@@ -0,0 +1,12 @@
+package juja.microservices.links.slackbot.integration;
+
+import org.junit.Test;
+
+public class LinksSlackbotIntegrationTest {
+
+    //TODO Replace with real test
+    @Test
+    public void test() throws Exception {
+    }
+}
+
