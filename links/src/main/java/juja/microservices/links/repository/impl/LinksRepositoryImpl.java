d48b4b76f20749880ba57cef78b3b64835bde580
Ivan Shapovalov <ivshapovalov@gmail.com>
2017-12-09 10:12:10 +0300

    #12 - Create project structure - Fix errors

--- links/src/main/java/juja/microservices/links/repository/impl/LinksRepositoryImpl.java
+++ links/src/main/java/juja/microservices/links/repository/impl/LinksRepositoryImpl.java
@@ -3,6 +3,8 @@ package juja.microservices.links.repository.impl;
 import juja.microservices.links.model.Link;
 import juja.microservices.links.repository.LinksRepository;
 
+import java.util.Set;
+
 /**
  * @author Ivan Shapovalov
  */
@@ -10,6 +12,11 @@ public class LinksRepositoryImpl implements LinksRepository {
 
     @Override
     public Link saveLink() {
+        return null;
+    }
+
+    @Override
+    public Set<Link> getAllLinks() {
         return null;
     }
 }
