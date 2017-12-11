72a80c6bcd88eac1d6d83f3006ec8b57ba5b003a
benjamin novikov <bigopendatanet@yahoo.com>
2017-12-12 00:27:03 +0200

    #11 SlackTextHandler URL links in text detection implemented

--- links-slackbot/src/main/java/juja/microservices/links/slackbot/util/SlackTextHandler.java
+++ links-slackbot/src/main/java/juja/microservices/links/slackbot/util/SlackTextHandler.java
@@ -2,17 +2,40 @@ package juja.microservices.links.slackbot.util;
 
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.HashSet;
 import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
- * @author Ivan Shapovalov
+ * @author Ben Novikov
  */
 @Slf4j
 public class SlackTextHandler {
 
     public static Set<String> getURLsFromText(String text) {
+        Set<String> result = new HashSet<>();
 
-        return null;
+        log.debug("Searching for URL links...");
+        Pattern pattern = Pattern.compile(
+                "\\b(((ht|f)tp(s?)\\:\\/\\/|~\\/|\\/)|www.)" +
+                        "(\\w+:\\w+@)?(([-\\w]+\\.)+(com|org|net|gov" +
+                        "|mil|biz|info|mobi|name|aero|jobs|museum" +
+                        "|travel|[a-z]{2}))(:[\\d]{1,5})?" +
+                        "(((\\/([-\\w~!$+|.,=]|%[a-f\\d]{2})+)+|\\/)+|\\?|#)?" +
+                        "((\\?([-\\w~!$+|.,*:]|%[a-f\\d{2}])+=?" +
+                        "([-\\w~!$+|.,*:=]|%[a-f\\d]{2})*)" +
+                        "(&(?:[-\\w~!$+|.,*:]|%[a-f\\d{2}])+=?" +
+                        "([-\\w~!$+|.,*:=]|%[a-f\\d]{2})*)*)*" +
+                        "(#([-\\w~!$+|.,*:=]|%[a-f\\d]{2})*)?\\b");
+
+        Matcher matcher = pattern.matcher(text);
+        while (matcher.find()) {
+            result.add(matcher.group());
+        }
+        log.debug("URL links found: [{}]", result.size());
+
+        return result;
     }
 }
 
--- links-slackbot/src/test/java/juja/microservices/links/slackbot/util/SlackTextHandlerTest.java
+++ links-slackbot/src/test/java/juja/microservices/links/slackbot/util/SlackTextHandlerTest.java
@@ -1,7 +1,43 @@
 package juja.microservices.links.slackbot.util;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+import static juja.microservices.links.slackbot.util.SlackTextHandler.getURLsFromText;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
 /**
- * @author Ivan Shapovalov
+ * @author Ben Novikov
  */
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class SlackTextHandlerTest {
+    @Test()
+    public void getURLsFromText_getSet_returnsExpectedSet() {
+        String text =
+                "Привет, вот ссылка http://mail.ru/page?parameter=value&also=another " +
+                "Привет, вот ссылка: mail.ru/page?parameter=value&also=another " +
+                "Привет, вот ссылка https://mail.ru Привет, вот ссылка <https://mail.ru> " +
+                "Привет, вот ссылка mail.ru Привет, вот ссылка <http://mail.ru|mail.ru> " +
+                "Привет, вот ссылка mail@mail.ru Привет, вот ссылка <mailto:mail.mail@mail.ru|mail@mail.ru> " +
+                "Привет, вот ссылка ftp://ivan.mail.ru Привет, вот ссылка <ftp://name:pass@warehouse.com>";
+        Set<String> expectedSet = new HashSet<>(Arrays.asList(
+                "http://mail.ru/page?parameter=value&also=another",
+                "https://mail.ru",
+                "http://mail.ru",
+                "ftp://ivan.mail.ru",
+                "ftp://name:pass@warehouse.com"));
+
+        Set<String> actualSet = getURLsFromText(text);
+
+        assertNotNull(actualSet);
+        assertEquals("Actual Set should be equal to expected", expectedSet, actualSet);
+    }
 }
