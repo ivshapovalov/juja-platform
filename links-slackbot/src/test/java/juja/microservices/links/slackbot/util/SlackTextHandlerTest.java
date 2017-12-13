935cba68a2647624afabf0b4308eaacdd2a40d97
benjamin novikov <bigopendatanet@yahoo.com>
2017-12-13 20:07:47 +0200

    #11 Test refactored to meet best practice

--- links-slackbot/src/test/java/juja/microservices/links/slackbot/util/SlackTextHandlerTest.java
+++ links-slackbot/src/test/java/juja/microservices/links/slackbot/util/SlackTextHandlerTest.java
@@ -9,42 +9,60 @@ import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 
 /**
+ * @author Ivan Shapovalov
  * @author Ben Novikov
  */
 public class SlackTextHandlerTest {
     private SlackTextHandler slackTextHandler;
-    private List<String> expected;
-    private String text;
 
     @Before
     public void setUp() {
         slackTextHandler = new SlackTextHandler();
-        text =  "Ссылка <> <http://google.com?parameter=value&also=another|google.com> " +
-                "Ссылка <1> http://google.com " +
-                "Ссылка <2 <yandex.ru/page?parameter=value&also=another> " +
-                "Ссылка 3> https://google.info " +
-                "Ссылка 4 <<https://mail.ru> " +
-                "Ссылка 5 <<> google.ya " +
-                "Ссылка 6 <http://yahoo.us|yahoo.us> " +
-                "Ссылка 7 <mail@microsoft.com " +
-                "Ссылка 8 <mailto:mail.mail@post.by|mail@post.by> " +
-                "Ссылка 9 ftp://ya.google.au> " +
-                "Ссылка 9 <ftp://ya.google.mail.ya.au> " +
-                "Ссылка 10 <ftp://name:pass@warehouse.com> ";
-        expected = new ArrayList<>(Arrays.asList(
-                "http://google.com?parameter=value&also=another",
-                "https://mail.ru",
-                "http://yahoo.us",
-                "ftp://ya.google.mail.ya.au",
-                "ftp://name:pass@warehouse.com"));
     }
 
     @Test
     public void getURLsFromText_withTextGiven_shouldReturnFiveLinks() {
+        //Given
+        String text =
+                "Case <1> http://google.com " +
+                        "Case <2 <yandex.ru/page?parameter=value&also=another> " +
+                        "Case 3> https://google.info " +
+                        "Case 4 <<https://mail.ru> " +
+                        "Case 5 <<> google.ya " +
+                        "Case 6 <http://yahoo.us|yahoo.us> " +
+                        "Case 7 <mail@microsoft.com " +
+                        "Case 8 <mailto:mail.mail@post.by|mail@post.by> " +
+                        "Case 9 ftp://ya.google.au> " +
+                        "Case 9 <ftp://ya.google.mail.ya.au> " +
+                        "Case 10 <ftp://name:pass@warehouse.com> " +
+                        "Case 11 <http:|ajdfhasdhfbks> " +
+                        "Case <> <http://google.com?parameter=value&also=another|google.com> ";
+        List<String> expected = new ArrayList<>(Arrays.asList(
+                "https://mail.ru",
+                "http://yahoo.us",
+                "ftp://ya.google.mail.ya.au",
+                "ftp://name:pass@warehouse.com",
+                "http://google.com?parameter=value&also=another"));
+
+        //When
         List<String> actual = slackTextHandler.getURLsFromText(text);
 
+        //Then
         assertThat(actual, IsIterableContainingInAnyOrder.containsInAnyOrder(expected.toArray()));
+    }
+
+    @Test
+    public void getURLsFromText_withEmptyText_shouldReturnEmptyList() {
+        //Given
+        String emptyText = "";
+
+        //When
+        List<String> actual = slackTextHandler.getURLsFromText(emptyText);
+
+        //Then
+        assertTrue(actual.isEmpty());
     }
 }
