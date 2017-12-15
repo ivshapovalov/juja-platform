c23ad7020b4948cdf8bb9696f6732c84224bfd91
Vadim Dyachenko <vadim.dyachenko@gmail.com>
2017-12-15 23:01:05 +0200

    #30 Added service integration test

--- slack-archive/src/test/java/juja/microservices/slack/integration/ArchiveServiceIntegrationTest.java
+++ slack-archive/src/test/java/juja/microservices/slack/integration/ArchiveServiceIntegrationTest.java
@@ -1,20 +1,23 @@
 package juja.microservices.slack.integration;
 
-import juja.microservices.slack.archive.Application;
+import com.lordofthejars.nosqlunit.annotation.UsingDataSet;
+import juja.microservices.slack.archive.model.ChannelDTO;
 import juja.microservices.slack.archive.repository.impl.ArchiveRepositoryImpl;
 import juja.microservices.slack.archive.service.impl.ArchiveServiceImpl;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.inject.Inject;
+import java.util.List;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = {Application.class})
-public class ArchiveServiceIntegrationTest {
+public class ArchiveServiceIntegrationTest extends BaseIntegrationTest {
 
     @Rule
     public ExpectedException expectedException = ExpectedException.none();
@@ -26,7 +29,15 @@ public class ArchiveServiceIntegrationTest {
     private ArchiveServiceImpl service;
 
     @Test
-    public void saveMessageTest() {
+    @UsingDataSet(locations = "/datasets/channels.json")
+    public void getChannelsTest() {
+        ChannelDTO channelOne = new ChannelDTO("CHANIDONE", "flood");
+        ChannelDTO channelTwo = new ChannelDTO("CHANIDTWO", "feedback");
 
+        List<ChannelDTO> actual = service.getChannels();
+
+        assertNotNull(actual);
+        assertTrue(actual.contains(channelOne));
+        assertTrue(actual.contains(channelTwo));
     }
 }
