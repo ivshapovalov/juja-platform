0592b4c5dc8a0f9ba52a9f96a95b6bec2d7aa3c9
Moses-Odessa <moses2804@gmail.com>
2017-12-19 15:24:27 +0200

    #15 add controller test

--- links/src/test/java/juja/microservices/links/controller/LinksControllerTest.java
+++ links/src/test/java/juja/microservices/links/controller/LinksControllerTest.java
@@ -1,7 +1,58 @@
 package juja.microservices.links.controller;
 
+import juja.microservices.links.model.Link;
+import juja.microservices.links.service.LinksService;
+import net.javacrumbs.jsonunit.core.Option;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static net.javacrumbs.jsonunit.fluent.JsonFluentAssert.assertThatJson;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
+import static org.springframework.http.MediaType.APPLICATION_JSON_UTF8;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import javax.inject.Inject;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * @author Ivan Shapovalov
+ * @author Vladimir Zadorozhniy
  */
+@RunWith(SpringRunner.class)
+@WebMvcTest(LinksController.class)
 public class LinksControllerTest {
+
+    @Inject
+    private MockMvc mockMvc;
+
+    @MockBean
+    private LinksService linksService;
+
+    private final String linksGetAllLinksUrl = "/v1/links";
+
+    @Test
+    public void testGetAllLinks() throws Exception {
+        Set<Link> expectedList = new HashSet(Arrays.asList(new Link("www.test1.com"), new Link("www.test2.net")));
+        when(linksService.getAllLinks()).thenReturn(expectedList);
+
+        String result = mockMvc.perform(get(linksGetAllLinksUrl)
+                .contentType(APPLICATION_JSON_UTF8))
+                .andExpect(content().contentType(APPLICATION_JSON_UTF8))
+                .andExpect(status().isOk())
+                .andReturn().getResponse().getContentAsString();
+
+        assertThatJson(result).when(Option.IGNORING_ARRAY_ORDER).isEqualTo(expectedList);
+        verify(linksService).getAllLinks();
+        verifyNoMoreInteractions(linksService);
+    }
 }
