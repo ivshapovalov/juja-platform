b07f6da82425f1374442d08b6631122c3479585d
Ivan Shapovalov <ivshapovalov@gmail.com>
2018-01-14 15:14:54 +0300

    #49 Update Repository Unit Tests (increase coverage)

--- links/src/test/java/juja/microservices/links/repository/LinksRepositoryTest.java
+++ links/src/test/java/juja/microservices/links/repository/LinksRepositoryTest.java
@@ -2,6 +2,7 @@ package juja.microservices.links.repository;
 
 import com.lordofthejars.nosqlunit.mongodb.InMemoryMongoDb;
 import com.lordofthejars.nosqlunit.mongodb.MongoDbRule;
+import juja.microservices.links.exception.NotFoundException;
 import juja.microservices.links.model.Link;
 import juja.microservices.links.repository.impl.LinksRepositoryImpl;
 import org.hamcrest.collection.IsIterableContainingInAnyOrder;
@@ -9,42 +10,140 @@ import org.junit.Before;
 import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.springframework.data.mongodb.core.MongoTemplate;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 import static com.lordofthejars.nosqlunit.mongodb.InMemoryMongoDb.InMemoryMongoRuleBuilder.newInMemoryMongoDbRule;
 import static com.lordofthejars.nosqlunit.mongodb.MongoDbRule.MongoDbRuleBuilder.newMongoDbRule;
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.Matchers.isEmptyString;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertThat;
-
+import static org.junit.Assert.assertTrue;
 
 /**
  * @author Ivan Shapovalov
  * @author Vladimir Zadorozhniy
  */
-
 public class LinksRepositoryTest {
 
     private static final String DB_NAME = "test-links";
     @ClassRule
     public static InMemoryMongoDb mongod = newInMemoryMongoDbRule().build();
     @Rule
+    final public ExpectedException expectedException = ExpectedException.none();
+    @Rule
     public MongoDbRule mongoRule = newMongoDbRule().defaultEmbeddedMongoDb(DB_NAME);
-    private List<Link> expectedList =
-            Arrays.asList(new Link("1", "www.test1.com"), new Link("2", "www.test2.net"));
+
     private LinksRepository linksRepository;
+
+    private MongoTemplate mongoTemplate;
 
     @Before
     public void setUp() {
-        MongoTemplate mongoTemplate = new MongoTemplate(mongoRule.getDatabaseOperation().connectionManager(), DB_NAME);
+        mongoTemplate = new MongoTemplate(mongoRule.getDatabaseOperation().connectionManager(), DB_NAME);
         linksRepository = new LinksRepositoryImpl(mongoTemplate, "links");
-        mongoTemplate.insert(expectedList, "links");
+        mongoTemplate.dropCollection("links");
     }
 
     @Test
-    public void testGetAllLinks() {
-        List<Link> result = linksRepository.getAllLinks();
-        assertThat(result, IsIterableContainingInAnyOrder.containsInAnyOrder(expectedList.toArray()));
+    public void getAllLinksWhenOneLinkIsHiddenExecutedCorrectly() {
+        //given
+        List<Link> originalData = Arrays.asList(
+                new Link("1", "www.test1.com", false),
+                new Link("3", "www.test3.com", true),
+                new Link("2", "www.test2.net", false));
+        mongoTemplate.insert(originalData, "links");
+
+        List<Link> expected = Arrays.asList(
+                new Link("1", "www.test1.com", false),
+                new Link("2", "www.test2.net", false));
+
+        //when
+        List<Link> actual = linksRepository.getAllLinks();
+
+        //then
+        assertThat(actual, IsIterableContainingInAnyOrder.containsInAnyOrder(expected.toArray()));
+    }
+
+    @Test
+    public void getAllLinksWhenAllLinksIsHiddenReturnsEmptyList() {
+        //given
+        List<Link> originalData = Arrays.asList(
+                new Link("1", "www.test1.com", true),
+                new Link("3", "www.test3.com", true),
+                new Link("2", "www.test2.net", true));
+        mongoTemplate.insert(originalData, "links");
+
+        //when
+        List<Link> actual = linksRepository.getAllLinks();
+
+        //then
+        assertTrue(actual.equals(new ArrayList<>()));
+    }
+
+    @Test
+    public void getAllLinksWhenDbIsEmptyReturnsEmptyList() {
+        //when
+        List<Link> actual = linksRepository.getAllLinks();
+
+        //then
+        assertTrue(actual.equals(new ArrayList<>()));
+    }
+
+    @Test
+    public void saveLinkExecutedCorrectly() {
+        //given
+        String url = "http://test.com";
+
+        //when
+        Link actual = linksRepository.saveLink(url);
+
+        //then
+        assertNotNull(actual);
+        assertThat(actual.getId(), not(isEmptyString()));
+        assertEquals(url, actual.getUrl());
+    }
+
+    @Test
+    public void getLinkByUrlWhenUrlNotExistsInDb() {
+        //given
+        String url = "www.test4.com";
+        List<Link> originalData = Arrays.asList(
+                new Link("1", "www.test1.com", false),
+                new Link("3", "www.test3.com", false),
+                new Link("2", "www.test2.net", false));
+        mongoTemplate.insert(originalData, "links");
+
+        expectedException.expect(NotFoundException.class);
+        expectedException.expectMessage(String.format("Not found link with url: [%s].", url));
+
+        //when
+        linksRepository.getLinkByURL(url);
+
+    }
+
+    @Test
+    public void getLinkByUrlWhenUrlAlreadyExistsInDb() {
+        //given
+        String url = "www.test1.com";
+        List<Link> originalData = Arrays.asList(
+                new Link("1", "www.test1.com", false),
+                new Link("3", "www.test3.com", false),
+                new Link("2", "www.test2.net", false));
+        mongoTemplate.insert(originalData, "links");
+
+        //when
+        Link actual = linksRepository.getLinkByURL(url);
+
+        //then
+        assertNotNull(actual);
+        assertThat(actual.getId(), not(isEmptyString()));
+        assertEquals(url, actual.getUrl());
     }
 }
