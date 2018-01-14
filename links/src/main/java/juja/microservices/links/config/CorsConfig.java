e6c3a3cabda4ca746ea4a5d57759dc0ba04755f0
Ivan Shapovalov <ivshapovalov@gmail.com>
2018-01-14 15:12:49 +0300

    #49 Fix error in CORS Filter

--- links/src/main/java/juja/microservices/links/config/CorsConfig.java
+++ links/src/main/java/juja/microservices/links/config/CorsConfig.java
@@ -16,8 +16,8 @@ public class CorsConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.addAllowedOrigin("");
-        config.addAllowedHeader("");
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
         config.addAllowedMethod("*");
         source.registerCorsConfiguration("/**", config);
         FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
