0f56ff26c3b7f4bd300fbf53be20c0ef4d04d309
Ivan Shapovalov <ivshapovalov@gmail.com>
2018-01-14 15:16:30 +0300

    #49 Add ApiModel to SaveLinkRequest

--- links/src/main/java/juja/microservices/links/model/SaveLinkRequest.java
+++ links/src/main/java/juja/microservices/links/model/SaveLinkRequest.java
@@ -2,23 +2,24 @@ package juja.microservices.links.model;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.ToString;
 import org.hibernate.validator.constraints.NotEmpty;
 
 @Getter
 @EqualsAndHashCode
+@ToString
+@ApiModel
 public class SaveLinkRequest {
     @NotEmpty
+    @ApiModelProperty(value = "URL of saved link", required = true)
     private String url;
 
     @JsonCreator
     public SaveLinkRequest(@JsonProperty("url") String url) {
         this.url = url;
-    }
-
-    @Override
-    public String toString() {
-        return String.format("Link[url='%s']", url);
     }
 }
