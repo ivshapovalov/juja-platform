4252c4578418e8a978a0a1b7987fbb0533970722
Ivan Shapovalov <ivshapovalov@gmail.com>
2018-01-14 15:33:52 +0300

    #55 Add 'Hide link' logic to LinksService

--- links-slackbot/src/main/java/juja/microservices/links/slackbot/service/impl/LinksSlackbotServiceImpl.java
+++ links-slackbot/src/main/java/juja/microservices/links/slackbot/service/impl/LinksSlackbotServiceImpl.java
@@ -1,16 +1,20 @@
 package juja.microservices.links.slackbot.service.impl;
 
 import juja.microservices.links.slackbot.exceptions.WrongCommandFormatException;
-import juja.microservices.links.slackbot.model.Link;
-import juja.microservices.links.slackbot.model.SaveLinkRequest;
+import juja.microservices.links.slackbot.model.links.HideLinkRequest;
+import juja.microservices.links.slackbot.model.links.Link;
+import juja.microservices.links.slackbot.model.links.SaveLinkRequest;
+import juja.microservices.links.slackbot.model.users.User;
 import juja.microservices.links.slackbot.repository.LinksRepository;
 import juja.microservices.links.slackbot.service.LinksSlackbotService;
+import juja.microservices.links.slackbot.service.UserService;
 import juja.microservices.links.slackbot.util.SlackTextHandler;
 import juja.microservices.links.slackbot.util.Utils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -23,12 +27,14 @@ public class LinksSlackbotServiceImpl implements LinksSlackbotService {
     private static final int EXPECTED_URL_AMOUNT_IN_TEXT = 1;
 
     private final LinksRepository linksRepository;
-
     private final SlackTextHandler slackTextHandler;
+    private final UserService userService;
 
-    public LinksSlackbotServiceImpl(LinksRepository linksRepository, SlackTextHandler slackTextHandler) {
+    public LinksSlackbotServiceImpl(LinksRepository linksRepository, SlackTextHandler slackTextHandler,
+                                    UserService userService) {
         this.linksRepository = linksRepository;
         this.slackTextHandler = slackTextHandler;
+        this.userService = userService;
     }
 
     @Override
@@ -39,8 +45,14 @@ public class LinksSlackbotServiceImpl implements LinksSlackbotService {
             throw new WrongCommandFormatException("Text must contains one url");
         } else {
             String url = new ArrayList<>(urls).get(0);
-            Link link = linksRepository.saveLink(new SaveLinkRequest(url));
-            return link;
+            return linksRepository.saveLink(new SaveLinkRequest(url));
         }
+    }
+
+    @Override
+    public Link hideLink(String fromSlackUser, String text) {
+        List<User> users = userService.findUsersBySlackUsers(Collections.singletonList(fromSlackUser));
+        String owner = users.get(0).getUuid();
+        return linksRepository.hideLink(new HideLinkRequest(owner, text));
     }
 }
